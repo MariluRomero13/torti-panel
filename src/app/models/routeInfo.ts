901352a64d20d44f76948e46733777633648eef0
MariluRomero13 <romeromarilu13@gmail.com>
2020-06-17 19:33:37 -0500

    [ADD] delivery route was added

--- src/app/models/routeInfo.ts
+++ src/app/models/routeInfo.ts
@@ -13,6 +13,7 @@ export const appRoutes: RouteInfo[] = [
   { path: '/panel/inventario', title: 'Inventario', icon: 'package-variant' },
   { path: '/panel/asignar-productos', title: 'Asignar productos', icon: 'swap-horizontal' },
   { path: '/panel/asignar-clientes', title: 'Asignar clientes', icon: 'account-switch' },
+  { path: '/panel/entregas', title: 'Entregas', icon: 'moped' },
   { path: '/panel/ventas', title: 'Ventas', icon: 'sale' },
   { path: '/panel/pagos-pendientes', title: 'Pagos pendientes', icon: 'currency-usd-off' },
   { path: '/panel/devoluciones', title: 'Devoluciones', icon: 'package-variant-closed' },
