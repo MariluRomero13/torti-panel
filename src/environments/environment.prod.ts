5041c8e43842c815ec51c98e9d51ea343c3599a8
MariluRomero13 <romeromarilu13@gmail.com>
2020-08-10 12:19:31 -0500

    [UPD] add 's' to websocket

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: `${window.location.origin}/api`,
-  apiUrlWS: `ws://${window.location.host}`
+  apiUrlWS: `wss://${window.location.host}`
 };
